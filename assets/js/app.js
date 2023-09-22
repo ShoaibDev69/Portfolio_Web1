5241b7b5845c5af1f057549063f2209289b54d34
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-22 16:55:13 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -238,7 +238,7 @@ let footer = $(`
       </svg>
     </a>
 
-    <a class="social-button github" href="#" target="_blank">
+    <a class="social-button github" href="https://github.com/ShoaibDev69" target="_blank">
     <!-- SVG code for Github icon -->
     <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
         <!-- body -->
