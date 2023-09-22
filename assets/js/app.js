b999e371c790999734a36c65917898cc76036eee
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-22 16:59:16 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -227,7 +227,7 @@ let footer = $(`
 
 
 
-    <a
+              <a
                 class="socialicon facebook"
                 href="https://www.facebook.com/profile.php?id=100089078420163"
                 target="_blank"
