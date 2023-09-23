a34918379ae19c60962c6e2f3be55f0d1fd0ecf8
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-23 16:27:12 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -227,15 +227,8 @@ let footer = $(`
 
 
 
-    <a class="social-button instagram" href="" target="_blank" >
-    <!-- svg code for instagram icon -->
-      <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
-        style="enable-background:new 0 0 202.5 202.5;">
-        <circle id="littleCircle" class="st0" cx="101" cy="101.5" r="18.9" />
-        <circle id="shutter" class="st0" cx="101" cy="101.5" r="8" />
-        <circle id="lens" class="st0" cx="125.5" cy="78.6" r="2.9" />
-        <path id="camera" class="st0" d="M79,60.5h44c10.5,0,19,8.5,19,19v44c0,10.5-8.5,19-19,19H79c-10.5,0-19-8.5-19-19v-44                                                                            C60,69,68.5,60.5,79,60.5z" />
-      </svg>
+    <a class="social-button facebook" href="https://www.facebook.com/profile.php?id=100089078420163" target="_blank" >
+    <i class="fab fa-facebook"></i>
     </a>
 
     <a class="social-button github" href="https://github.com/ShoaibDev69" target="_blank">
