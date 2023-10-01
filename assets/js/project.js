aa365ee70afb0ffcb97a6cf83b386fe05bba3f4b
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-10-01 14:29:52 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -14,8 +14,8 @@ const projects = [
     Githublink: "",
   },
   {
-    title: "Chess Engine",
-    cardImage: "assets/images/project-page/chess.jpg",
+    title: "Metaverse Horizon",
+    cardImage: "https://i.gifer.com/origin/68/68395474667ae256ef34e61799344f6b.gif",
     description: "A chess engine for a popular game dev engine.",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
