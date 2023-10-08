88806e8f1972047c114c788181ba4c3974858faa
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-10-08 15:54:33 +0530

    Update Project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,7 +7,7 @@ const projectcards = document.querySelector(".projectcards");
 const projects = [
   {
     title: "CoinReach",
-    cardImage: "assets/images/project-page/coinreach.gif",
+    cardImage: "assets/images/project-page/coinreach.png",
     description: "A Crpto Sending Project",
     // tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "https://coin-reach-project.vercel.app/",
